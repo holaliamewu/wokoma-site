7252337e4530c0fe65c7fe964d82330aca7cc319
holaliamewu <me.amewu@gmail.com>
2023-09-01 06:25:46 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function App() {
+  return(
+    <div id='main--container'>
+      <header>
+        <div id='logo'>Wokoma</div>
+      </header>
+      <section>
+        <h1>Oh Sorry.</h1>
+        <h3>This page will go live soon.</h3>
+      </section>
+
+      <footer>
+        <a href='' target='_blank'>
+          <i class="fa-brands fa-twitter"></i>
+        </a>
+        <span>Give us a follow to stay updated, who knows something can come up tomorrow.</span>
+      </footer>
+    </div>
+  ) 
+}
